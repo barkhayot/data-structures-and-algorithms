6f9612ce84e966173fe73052ddb2c261568eba8d
Barkhayot Juraev <barkhayotoff@gmail.com>
2023-09-10 14:31:35 +0900

    Create stack.py

---
+++ stack/stack.py
@@ -0,0 +1,49 @@
+# STACK DS Implementation
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+    
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new = Node(value)
+        
+        if self.height == 0:
+            self.top = new
+        else:
+            new.next = self.top
+            self.top = new
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+
+        return temp
+
+
+
+my_stack = Stack(4)
+my_stack.push(3)
+
+my_stack.push(1)
+my_stack.print_stack()
+my_stack.pop()
+my_stack.print_stack()
